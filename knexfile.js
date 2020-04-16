99bebd943e70e171ee5cf8d5b9239d51be775c31
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-16 16:33:33 -0600

    created database using migrate file

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,12 @@ module.exports = {
       filename: './data/recipes.db3'
     },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
     migrations: {
       directory: "./data/migrations,"
     },
